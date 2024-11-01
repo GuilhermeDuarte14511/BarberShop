bca56c76141f2289624895a7e052c9065dd2fbed
Guilherme Duarte de Oliveira <guilherme.oliveira@granitopagamentos.com.br>
2024-11-01 16:54:25 -0300

    AJUSTES

--- BarberShop/Controllers/AgendamentoController.cs
+++ BarberShop/Controllers/AgendamentoController.cs
@@ -66,7 +66,7 @@ namespace BarberShopMVC.Controllers
             return View(agendamento);
         }
 
-        public IActionResult Create()
+        public async Task<IActionResult> CreateAsync()
         {
             await LogAsync("INFO", nameof(Create), "Tela de criação de agendamento acessada", "");
             ViewBag.Clientes = _clienteRepository.GetAllAsync();
