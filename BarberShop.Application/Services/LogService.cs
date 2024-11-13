73cfd10fac2bca0cbdee18867eeb7717b1cfd546
Guilherme Duarte <62967603+GuilhermeDuarte14511@users.noreply.github.com>
2024-11-13 11:04:30 -0300

    ajustes log

--- BarberShop.Application/Services/LogService.cs
+++ BarberShop.Application/Services/LogService.cs
@@ -1,6 +1,10 @@
 ﻿using BarberShop.Domain.Entities;
 using BarberShop.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace BarberShop.Application.Services
 {
@@ -8,11 +12,13 @@ namespace BarberShop.Application.Services
     {
         private readonly BarbeariaContext _context;
         private readonly ILogger<LogService> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public LogService(BarbeariaContext context, ILogger<LogService> logger)
+        public LogService(BarbeariaContext context, ILogger<LogService> logger, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task SaveLogAsync(string logLevel, string source, string message, string data = null, string resourceId = null)
@@ -21,18 +27,25 @@ namespace BarberShop.Application.Services
             {
                 _logger.LogDebug("Salvando log no banco de dados...");
 
+                // Obtenha o usuarioId dos claims, se estiver disponível
+                var usuarioIdClaim = _httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier);
+                var usuarioId = usuarioIdClaim != null ? usuarioIdClaim.Value : "Desconhecido";
+
                 // Validações e atribuições de valores padrão
                 logLevel = string.IsNullOrWhiteSpace(logLevel) ? "Information" : logLevel;
                 source = string.IsNullOrWhiteSpace(source) ? "Source não especificado" : source;
                 message = string.IsNullOrWhiteSpace(message) ? "Mensagem não fornecida" : message;
-                data = data ?? DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm");
+                data = data ?? DateTime.UtcNow.ToString("dd/MM/yyyy HH:mm:ss");
                 resourceId = string.IsNullOrWhiteSpace(resourceId) ? "N/A" : resourceId;
+
+                // Adiciona o usuarioId ao início da mensagem
+                var formattedMessage = $"Usuario Id {usuarioId}, {message}";
 
                 var logEntry = new Log
                 {
                     LogLevel = logLevel,
                     Source = source,
-                    Message = message,
+                    Message = formattedMessage,
                     Data = data,
                     ResourceID = resourceId,
                     LogDateTime = DateTime.UtcNow
@@ -44,11 +57,9 @@ namespace BarberShop.Application.Services
             }
             catch (Exception ex)
             {
-                // Log de erro para o logger interno, com detalhes para rastrear o problema.
                 _logger.LogError($"Erro ao salvar log no banco de dados: {ex.Message}");
                 throw new Exception("Erro ao salvar log no banco de dados.");
             }
         }
-
     }
 }
