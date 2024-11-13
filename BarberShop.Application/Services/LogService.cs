6c592c503396c9b87b659837524f1abd38f53baa
Guilherme Duarte <62967603+GuilhermeDuarte14511@users.noreply.github.com>
2024-11-13 11:00:49 -0300

    ajustes log

--- BarberShop.Application/Services/LogService.cs
+++ BarberShop.Application/Services/LogService.cs
@@ -15,22 +15,40 @@ namespace BarberShop.Application.Services
             _logger = logger;
         }
 
-        public async Task SaveLogAsync(string logLevel, string source, string message, string data, string resourceId = null)
+        public async Task SaveLogAsync(string logLevel, string source, string message, string data = null, string resourceId = null)
         {
-            _logger.LogDebug("Salvando log no banco de dados...");
-            var logEntry = new Log
+            try
             {
-                LogLevel = logLevel,
-                Source = source,
-                Message = message,
-                Data = data,
-                ResourceID = resourceId,
-                LogDateTime = DateTime.UtcNow
-            };
+                _logger.LogDebug("Salvando log no banco de dados...");
 
-            _context.Logs.Add(logEntry);
-            await _context.SaveChangesAsync();
-            _logger.LogDebug("Log salvo com sucesso.");
+                // Validações e atribuições de valores padrão
+                logLevel = string.IsNullOrWhiteSpace(logLevel) ? "Information" : logLevel;
+                source = string.IsNullOrWhiteSpace(source) ? "Source não especificado" : source;
+                message = string.IsNullOrWhiteSpace(message) ? "Mensagem não fornecida" : message;
+                data = data ?? DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm");
+                resourceId = string.IsNullOrWhiteSpace(resourceId) ? "N/A" : resourceId;
+
+                var logEntry = new Log
+                {
+                    LogLevel = logLevel,
+                    Source = source,
+                    Message = message,
+                    Data = data,
+                    ResourceID = resourceId,
+                    LogDateTime = DateTime.UtcNow
+                };
+
+                _context.Logs.Add(logEntry);
+                await _context.SaveChangesAsync();
+                _logger.LogDebug("Log salvo com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                // Log de erro para o logger interno, com detalhes para rastrear o problema.
+                _logger.LogError($"Erro ao salvar log no banco de dados: {ex.Message}");
+                throw new Exception("Erro ao salvar log no banco de dados.");
+            }
         }
+
     }
 }
--- BarberShop.Domain/Entities/Log.cs
+++ BarberShop.Domain/Entities/Log.cs
@@ -4,12 +4,12 @@
     {
         public class Log
         {
-            public int LogId { get; set; }
-            public DateTime LogDateTime { get; set; }
-            public string LogLevel { get; set; }
-            public string Source { get; set; }
-            public string Message { get; set; }
-            public string Data { get; set; }
-            public string ResourceID { get; set; } // Nova propriedade para armazenar o ID do recurso
+            public int? LogId { get; set; }
+            public DateTime? LogDateTime { get; set; }
+            public string? LogLevel { get; set; }
+            public string? Source { get; set; }
+            public string? Message { get; set; }
+            public string? Data { get; set; }
+            public string? ResourceID { get; set; } // Nova propriedade para armazenar o ID do recurso
         }
     }
